6f37c7670fe960246c583a1ae2e4439cc103960a
Priscila Cabral <priscilacabral.dev@gmail.com>
2020-05-12 19:16:56 -0300

    create edit function and redirect to root page after that

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,8 +5,17 @@ class TasksController < ApplicationController
     def create
         @task = Task.new(params.require(:task).permit(:description, :status))
 
-        @task.save
+        @task.save 
         redirect_to root_path
     end
     
+    def edit
+        @task = Task.find(params[:id])
+    end
+   
+    def update
+        @task = Task.find(params[:id])
+        @task.update(params.require(:task).permit(:description, :status))
+        redirect_to root_path
+    end
 end
