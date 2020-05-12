d92b43680cf6c24610d41c35dc5463b45d3f4fcc
Priscila Cabral <priscilacabral.dev@gmail.com>
2020-05-11 21:59:00 -0300

    create a home model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
     def welcome
+        @tasks = [ ]
+        @tasks << Task.new('Passear com o dog')
+        @tasks << Task.new('Ir no mercado')
     end
 end
